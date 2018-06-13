87879a38207e3717982ef1938e1a8e4d96f90b5b
Karow <karow.maruf14@imperial.ac.uk>
2018-06-13 16:09:22 +0100

    - fixed bug in retrieving status

--- sdk/src/main/scala/ec/iex/core/Work.scala
+++ sdk/src/main/scala/ec/iex/core/Work.scala
@@ -59,7 +59,7 @@ object Work {
   def waitUntilCompleted(web3: EthereumSynchronous, workId: String, credentials: Credentials, gasPrice: BigInteger, gasLimit: BigInteger) = {
     val workInfo = WorkOrder.load(workId, web3.web3j, credentials, gasPrice, gasLimit)
 
-    while (workInfo.m_status() != 4) {
+    while (workInfo.m_status().send().intValue() != 4) {
       Thread.sleep(5000)
     }
 
