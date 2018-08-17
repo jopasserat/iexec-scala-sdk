a0bb937e7ecca77cea3c36124579b35294696530
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2018-08-17 18:17:00 +0100

    fix: implicit numeric widening in Work.scala

--- sdk/src/main/scala/ec/iex/core/Work.scala
+++ sdk/src/main/scala/ec/iex/core/Work.scala
@@ -28,9 +28,11 @@ object WorkState extends Enumeration {
 
 object Work {
 
+  import ec.iex.util._
+
   def buyWorkOrder(iexecHubContract: IexecHub, workerPoolId: Int, workerPoolAddress: String, appAddress: String, datasetAddress: String, parameters: String, callbackAddress: String, beneficiaryAddress: String) = {
 
-    val txReceipt = iexecHubContract.buyForWorkOrder(BigInteger.valueOf(workerPoolId), workerPoolAddress, appAddress, datasetAddress, parameters, callbackAddress, beneficiaryAddress).send()
+    val txReceipt = iexecHubContract.buyForWorkOrder(workerPoolId, workerPoolAddress, appAddress, datasetAddress, parameters, callbackAddress, beneficiaryAddress).send()
 
     val workId = "0x" + txReceipt.getLogs().get(1).getTopics().get(1).replace("0x", "").replaceFirst("^0+(?!$)", "")
 
